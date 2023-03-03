e7d6f61ddb7d8012504ba793d88659a496cb7460
rahulkarmakaritobuz <rahul.karmakar@itobuz.com>
2023-03-03 18:40:35 +0530

    module created

---
+++ modules/index.mjs
@@ -0,0 +1,7 @@
+// import {add,sub} from './math.mjs';
+import * as rahul from './math.mjs';
+
+// let add=require('./math');
+// let x=math.add(1,3);
+console.log(rahul.add(1,3));
+console.log(rahul.sub(1,3));
